ee8a7374bd5181c29c457444a84a16a594f77112
Simon Schmidt <simon.schmidt@idealo.de>
2018-01-23 18:27:00 +0100

    restructure of functions into clusters

--- addons/grad_telephone/CfgFunctions.h
+++ addons/grad_telephone/CfgFunctions.h
@@ -2,17 +2,95 @@ class CfgFunctions
 {
 	class grad_telephone {
 
-		class misc
+		class calling
 		{
-			file = "\grad_telephone\functions";
+			file = "\grad_telephone\functions\calling";
 
+			class callAccept;
+			class callDialing;
+			class callEnding;
+			class callReceiving;
+			class callReject;
+			class callTalking;
+			class callWaiting;
+			class callWaitingDroppedPhone;
+
+			class pleaseReceive;
+			class setCallersPhoneFrequency;
+		};
+
+		class celltower
+		{
+			file = "\grad_telephone\functions\celltower";
+
+			class cellTowerCalculateTFARMultiplicator;
+			class cellTowerGetAttendeesDistance;
+			class cellTowerMarkerCreate;
+			class cellTowerRefreshSignalStrength;
+			class cellTowerSignalStrengthCheck;
+			class cellTowerSpawn;
+			class isCellTowerReachable;
+		};
+
+		class core
+		{
+			file = "\grad_telephone\functions\core";
+
+			class generatePhoneNumber;
+			class getNativePhoneCode;
+			class getNativePhoneFrequency;
 			class getRadio;
+			class getUniquePhoneNumber;
+
 			class initAll { postInit = 1; };
 			class initClient;
 			class initServer;
-			class isCellphone;
 
-			
+			class isCellphone;
+			class setNativePhoneFrequency;
+
 		};
+
+		class ied {
+			file = "\grad_telephone\functions\ied";
+
+			class callIED;
+			class destroyIED;
+			class isSelectedContactIED;
+			class programIED;
+		}
+
+		class interaction {
+			file = "\grad_telephone\functions\interaction";
+
+			class interactGiveNumber;
+			class interactHasNumber;
+			class interactReceiveNumber;
+			class playGiveAnimation;
+		}
+
+		class ui {
+			file = "\grad_telephone\functions\ui";
+
+			class getPhoneContactList;
+			class getSelectedContactName;
+			class getSelectedContactObject;
+			class getSelectedContactRadioID;
+			class isNotInContacts;
+			class restoreDisplay;
+			class setDisplayName;
+			class setDisplayNumber;
+			class setPhoneContactList;
+			class showHint;
+			class showHintCall;
+			class showHintUnlimited;
+			class showNextContact;
+			class showNextMenuEntry;
+			class showRadioInfo;
+			class updateContactList;
+
+		}
+
+
 	};
 };
